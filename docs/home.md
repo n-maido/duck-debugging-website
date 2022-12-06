a9e18b733f265dd0ecb706cf85200d4d324218f3
n-maido <nhi_maido@outlook.com>
2022-12-05 23:00:04 -0800

    add zip dl link

---
+++ docs/home.md
@@ -0,0 +1,38 @@
+---
+sidebar_position: 1
+slug: /
+---
+
+# Duck Debugging
+
+A platform for programmers to exchange ideas and post questions
+
+## Original Pitch
+
+[Watch our original pitch video](https://youtu.be/dAlHogZCGOw)
+
+## Team Effort Breakdown
+
+### Mohammad
+
+### Leo
+
+### Kowsar
+
+### Nhi
+
+## App APK
+
+<!-- <a href="https://n-maido.github.io/duck-debugging-website/assets/duck-debugging.apk" download>Download the app APK</a> -->
+
+## MVVM and Threaded Design Diagrams
+
+## Presentation Video
+
+Watch our final presentation video
+
+## Source Code
+
+<a href="https://n-maido.github.io/duck-debugging-website/assets/duck-debugging.zip" download>Download a zip of the project</a>
+
+[View the GitLab repository](https://csil-git1.cs.surrey.sfu.ca/mjt19/duck-debugging/-/tree/master)
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,27 +10,23 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
   baseUrl: '/duck-debugging-website/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/duck.png',
   organizationName: 'n-maido', // Usually your GitHub org/user name.
   projectName: 'duck-debugging-website', // Usually your repo name.
   deploymentBranch: 'gh-pages',
-
+ 
   presets: [
     [
       '@docusaurus/preset-classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          // editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
-        },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -41,71 +37,43 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      image: 'img/duck.png',
       navbar: {
-        title: 'My Site',
+        title: 'Duck Debugging',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Duck Debugging Logo',
+          src: 'img/duck.png',
         },
         items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+          // {
+          //   type: 'doc',
+          //   docId: 'intro',
+          //   position: 'left',
+          //   label: 'Tutorial',
+          // },
+          // {to: '/blog', label: 'Blog', position: 'left'},
+          // {
+          //   href: 'https://github.com/facebook/docusaurus',
+          //   label: 'GitHub',
+          //   position: 'right',
+          // },
         ],
       },
       footer: {
         style: 'dark',
         links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
+          // {
+          //   title: 'Docs',
+          //   items: [
+          //     {
+          //       label: 'Tutorial',
+          //       to: '/docs/intro',
+          //     },
+          //   ],
+          // },
+        
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Duck Debugging, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
